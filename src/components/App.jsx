2488f0b5ef6c6fbe443f52eb811f3b9e39426bd0
Lau Kin Peng <laukinpeng@gmail.com>
2024-01-19 00:07:54 +0800

    initial data population

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,41 @@
-function App() {
-  return <div>{/* Complete the exercise here. */}</div>;
-}
+import React, { useState, useEffect } from 'react';
 
-export default App;
+export default function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    let blue = async () => {
+      try {
+        const response = await fetch('/api/posts');
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data. Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setPosts(data.posts);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    blue();
+  }, []);
+
+  console.log('postspostsposts', posts);
+
+  return (
+    <>
+      dcmopaswkljncmlesdkhjvfkl;h
+      {posts.map((post) => (
+        <div>
+          <h1>{post.id}</h1>
+          <h2>{post.author.name}</h2>
+          <h3>{post.title}</h3>
+          <img src={post.author.avatar} />
+          <p>Date: {post.publishDate}</p>
+          <p>{post.summary}</p>
+        </div>
+      ))}
+    </>
+  );
+}
